15346b04e816aea0e505700aacec9ab1cde117f7
Immy <mr.imrankhan@gmail.com>
2023-12-24 15:15:20 +0500

    final update for this bit will come back to it later

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const port = 3000
 
 app.set('view engine', 'ejs');
 let blogPosts = [
-    { id: 1, title: 'First Post', content: 'This is the first post.' },
+    {id: 1, title: "Post 1 Title",  content: "The content of the first post"  },
     // more posts...
   ];
+
   
 
 app.use(express.urlencoded({ extended: true }));
@@ -17,26 +18,34 @@ app.use(express.static('public'));
 
 //This middleware is necessary to parse form data sent through HTTP POST requests.
 app.use(bodyParser.urlencoded({ extended: true }));
-
+  
 
 
 // Home Route
 app.get('/', (req, res) => {
-    res.render('index', { blogs: blogPosts });
+
+    // Sort posts by date in descending order (newest first)
+    const sortedPosts = blogPosts.sort((a, b) => b.date - a.date);
+
+    res.render('index', { blogs: sortedPosts });
+    // res.render('index', { blogs: blogPosts });
 });
 
 // Create Route
-app.get('/create', (req, res) => {
-    res.render('create');
+app.get('create', (req, res) => {
+    res.render('/');
 });
 
 // Save Route
 app.post('/post', (req, res) => {
     // Add new blog post to the array or file
+    // const d = new Date() ;
+    const blogTime = d.getDate("1");
     const newPost = {
         id: blogPosts.length + 1, // // Simple ID assignment, might need a better approach for unique IDs
         title: req.body.title,
-        content: req.body.content
+        content: req.body.content,
+        date: new Date() // Add the current date
     };
     blogPosts.push(newPost); // add the new post to blogPosts array (otherwise write to a file or database in)
     res.redirect('/');
